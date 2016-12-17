b6e0877d4724840c5d93d6bc5685cea81dbbeaec
Carlos Escobar <carlos@LosBombz.local>
2016-12-17 16:55:15 -0500

    adding eslintrc an gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": "standard",
+    "installedESLint": true,
+    "parser": "babel-eslint",
+    "plugins": [
+        "standard",
+        "promise",
+        "react"
+    ],
+    rules: {
+        "indent": ["error", 4],
+        "space-before-function-paren": 0,
+        "no-unused-vars": 0,
+        "semi": 0,
+        "no-useless-constructor": 0,
+        "arrow-spacing": 0,
+        "no-new": 0,
+        "no-trailing-spaces": 0
+    }
+};
