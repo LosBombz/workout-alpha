c342bfccb603efbf8330ac279709b7d960647d57
Carlos Escobar <carlos.b.escobar@gmail.com>
2016-12-18 18:30:35 -0500

    making a compoent structure with react

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>What up world</div>
+        );
+    }
+}
+
+export default App;
