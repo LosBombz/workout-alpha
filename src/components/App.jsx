7a4d326fc30cac6c219b20705c927c75e443749a
Carlos Escobar <carlos.b.escobar@gmail.com>
2016-12-19 00:12:00 -0500

    adding init code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,15 @@
 import React from 'react';
+import firebase from 'firebase';
+
+const app = firebase.initializeApp({
+    apiKey: 'AIzaSyBADGH-KXqmKHxn6ZVJr3XcpV5MLYK4XRM',
+    authDomain: 'workout-alpha.firebaseapp.com',
+    databaseURL: 'https://workout-alpha.firebaseio.com',
+    storageBucket: 'workout-alpha.appspot.com',
+    messagingSenderId: '873329747843'
+});
+
+console.log(app);
 
 class App extends React.Component {
     constructor() {
