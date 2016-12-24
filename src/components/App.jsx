899ae1336ce06e623c645522c82ff5cc5d8bda51
Carlos Escobar <carlos.b.escobar@gmail.com>
2016-12-24 14:13:35 -0500

    getting data from the database

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,61 @@
+import _ from 'lodash';
 import React from 'react';
 import firebase from 'firebase';
 
 const app = firebase.initializeApp({apiKey: 'AIzaSyBADGH-KXqmKHxn6ZVJr3XcpV5MLYK4XRM', authDomain: 'workout-alpha.firebaseapp.com', databaseURL: 'https://workout-alpha.firebaseio.com', storageBucket: 'workout-alpha.appspot.com', messagingSenderId: '873329747843'});
 
 const provider = new firebase.auth.GoogleAuthProvider();
-
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        // User is signed in.
-        console.log(user.displayName, '| is still signed in');
-    } else {
-        // No user is signed in.
-        console.log('no user is signed in');
-    }
-});
+const database = firebase.database();
 
 console.log(app);
 
+function getExercises() {
+    let user = firebase.auth().currentUser;
+    console.log(user);
+    return firebase.database().ref('/exercises/').once('value').then(function(snapshot) {
+        let exercises = snapshot.val();
+        console.log(exercises);
+        // ...
+    });
+}
+
+var user;
+
 class App extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {
+            exercises: []
+        }
+
+        firebase.auth().onAuthStateChanged((user)=> {
+            if (user) {
+                // User is signed in.
+                console.log(user.displayName, '| is still signed in');
+                user = firebase.auth().currentUser;
+                console.log(user);
+                return firebase.database().ref('/exercises/').once('value').then((snapshot)=> {
+                    let res = snapshot.val();
+                    _.forEach(res, (exercise, idx)=> {
+                        this.state.exercises.push(
+                            <li key={idx}>
+                                <h2>{exercise.name}</h2>
+                                <div>type: {exercise.type}</div>
+                            </li>
+                        );
+                    });
+                    console.log(this.state.exercises);
+
+                    this.setState({
+                        exercises: this.state.exercises
+                    });
+                    // ...
+                });
+            } else {
+                // No user is signed in.
+                console.log('no user is signed in');
+            }
+        });
     }
 
     login() {
@@ -44,7 +80,13 @@ class App extends React.Component {
     }
 
     logout() {
-
+        firebase.auth().signOut().then(function() {
+            console.log('successfuly logged out');
+            // Sign-out successful.
+        }, function(error) {
+            console.error(error);
+            // An error happened.
+        });
     }
 
     render() {
@@ -54,6 +96,10 @@ class App extends React.Component {
                 <div>nah chill</div>
                 <button onClick={this.login.bind(this)}>Login with google</button>
                 <button>Logout</button>
+                <ul>
+                    {this.state.exercises}
+                </ul>
+
             </div>
 
         );
