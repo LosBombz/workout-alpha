18794d3344d7ed8995eda6ad117539ff20a47eff
Carlos Escobar <carlos.b.escobar@gmail.com>
2016-12-24 14:13:53 -0500

    starting a list component

---
+++ src/components/ExerciseList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ExerciseList extends React.Component {
+    render() {
+        return (
+            <ul></ul>
+        );
+    }
+}
+
+export default ExerciseList;
